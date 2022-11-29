d05909439ea247aeb822f0dfddd8b9a2e2526765
Kweeka <mtalbi29@yahoo.com>
2022-11-29 13:21:44 +0100

    Refactor App.js to a functional component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Calculator from './Components/Calculator/Calculator';
 import calculate from './Utils/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const App = () => {
+  const [params, setParams] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(ev) {
-    this.setState((prevState) => calculate(prevState, ev.target.textContent));
-  }
+  const handleClick = (ev) => {
+    setParams((prevState) => calculate(prevState, ev.target.textContent));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    const screenText = `${total ?? ''} ${operation ?? ''} ${next ?? ''}`;
-    return (
-      <div className="App">
-        <Calculator screenText={screenText} calculate={(ev) => this.handleClick(ev)} />
-      </div>
-    );
-  }
-}
+  const { total, next, operation } = params;
+  const screenText = `${total ?? ''} ${operation ?? ''} ${next ?? ''}`;
+  return (
+    <div className="App">
+      <Calculator screenText={screenText} calculate={(ev) => handleClick(ev)} />
+    </div>
+  );
+};
 
 export default App;
